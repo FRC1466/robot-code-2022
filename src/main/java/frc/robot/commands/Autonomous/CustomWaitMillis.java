b2ddcdd34f142ab5d534ce20e23f09eb46ec6d3c
7910f6ba7ee4 <vyn.sky@protonmail.com>
2022-09-15 21:21:03 -0400

    cutom wait

--- src/main/java/frc/robot/commands/Autonomous/CustomWaitMillis.java
+++ src/main/java/frc/robot/commands/Autonomous/CustomWaitMillis.java
@@ -7,30 +7,44 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class CustomWait extends CommandBase {
-    private int iteration = 0;
+
+public class CustomWaitMillis extends CommandBase {
     private final DriveSubsystem m_drive;
     private int m_milliseconds;
-
-    CustomWait(DriveSubsystem drive, int milliseconds) {
+    private long startTime;
+    private long endTime;
+    
+    /**
+     * Waits for some number of ms while calling DifferentialDrive.
+     * @param drive drive subsystem
+     * @param milliseconds waiting time in milliseconds
+     */
+    CustomWaitMillis(DriveSubsystem drive, int milliseconds) {
         m_drive = drive;
         m_milliseconds = milliseconds;
+
+        
+    }
+
+    @Override
+    public void initialize() {
+        startTime = System.currentTimeMillis();
+        endTime = startTime + m_milliseconds;
     }
 
     @Override
     public void execute() {
-        iteration++;
         m_drive.pacifyDrive();
 
     }
 
     @Override
     public boolean isFinished() {
-        if (iteration*20 > m_milliseconds) {
+        if (System.currentTimeMillis() < endTime) {
             return true;
         } else {
             return false;
         }
 
     }
-}
+} 
